a141c8bcd6067541381e7f9ded67710f96835326
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-14 09:19:50 +0700

    update script

--- startGetPrice.sh
+++ startGetPrice.sh
@@ -1,1 +1,2 @@
-java -jar target/TaoProject-1.0-SNAPSHOT.jar GET_PRICE ./src/main/resources/application.property
+java -jar target/TaoProject-1.0-SNAPSHOT.jar GET_PRICE ./src/main/resources/application.property >> logs/getPrice.log &
+tail -100f logs/getPrice.log
