510cc867b5bb2d8306822a7cf0666adb32954adc
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-14 16:32:19 +0700

    remove tail log

--- startAutoRegister.sh
+++ startAutoRegister.sh
@@ -1,2 +1,2 @@
 java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property >> logs/autoRegister.log &
-tail -100f logs/autoRegister.log
+#tail -100f logs/autoRegister.log
