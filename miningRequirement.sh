cdeaf247e8cce8b1fea8be16175ef5d557c60ca5
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-15 23:44:22 +0700

    update install-hotkey-all.sh

--- miningRequirement.sh
+++ miningRequirement.sh
@@ -11,10 +11,10 @@ cd ..
 git clone https://github.com/opentensor/subtensor.git
 cd subtensor
 sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
-
+pwd
 
 #install new driver
 cd ..
 sudo curl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/550.54.14/NVIDIA-Linux-x86_64-550.54.14.run
-chmod 777 NVIDIA-Linux-x86_64-550.54.14.run
+sudo chmod 777 NVIDIA-Linux-x86_64-550.54.14.run
 sudo ./NVIDIA-Linux-x86_64-550.54.14.run
