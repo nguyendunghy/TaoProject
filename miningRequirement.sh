b8d389597d23dbece1698507a730e6415cfa7435
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-15 22:51:32 +0700

    add miningRequirement.sh

---
+++ miningRequirement.sh
@@ -0,0 +1,20 @@
+#install bittensor
+pip install --upgrade pip
+pip3 install bittensor
+
+#setup coldkey and hotkey
+./install-keys/install-coldkey.sh
+./install-keys/install-hotkey-all.sh
+
+#install subtensor
+cd ..
+git clone https://github.com/opentensor/subtensor.git
+cd subtensor
+sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
+
+
+#install new driver
+cd ..
+sudo curl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/550.54.14/NVIDIA-Linux-x86_64-550.54.14.run
+chmod 777 NVIDIA-Linux-x86_64-550.54.14.run
+sudo ./NVIDIA-Linux-x86_64-550.54.14.run
