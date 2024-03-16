6b0ac350d02bd17d5f3e7f4d319277392799f3b7
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-16 08:49:36 +0700

    add code to get ip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,19 @@
 package org.example;
 
-import org.example.engine.AutoRegisterEngine;
-import org.example.engine.GetPriceEngine;
-import org.example.monitor.MonitorImpl;
-import org.example.utils.Constants;
-import org.example.utils.PropertyUtils;
-
-import java.util.Arrays;
+import org.example.utils.SystemUtils;
 
 public class Main {
     public static void main(String[] args) {
-//        args = new String[]{"MONITOR", "/Users/nannan/IdeaProjects/TaoProject/src/main/resources/application.property"};
-
-        if (args == null || args.length < 2) {
-            System.out.println("Missing argument, the first argument is GET_PRICE,AUTO_REGISTER or MONITOR. The second argument is property file path");
+        try {
+            System.out.println("Public ip" + SystemUtils.getPublicIp());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        Constants.CONFIG_FILE = args[1];
-        String[] subnetIdArray;
-        switch (args[0]) {
-            case "GET_PRICE":
-                subnetIdArray = PropertyUtils.getProperty("get.price.subnetId").split(",");
-                GetPriceEngine.startRunningGetPrice(Arrays.asList(subnetIdArray));
-                break;
-            case "AUTO_REGISTER":
-                subnetIdArray = PropertyUtils.getProperty("register.subnetId").split(",");
-                AutoRegisterEngine.startRunningAutoRegisterEngine(Arrays.asList(subnetIdArray));
-                break;
-            case "MONITOR":
-                MonitorImpl monitor = new MonitorImpl();
-                monitor.run();
-                break;
-            default:
-                break;
+
+        try {
+            System.out.println("IP socket" + SystemUtils.getPublicIpBySocket());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
     }
