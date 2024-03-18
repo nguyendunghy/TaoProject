01c06871cfa5ec60645075ff3ff1cd4e384261f3
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-18 11:32:28 +0700

    upgrade setup subnet 26

--- subnet-setup.sh
+++ subnet-setup.sh
@@ -4,6 +4,10 @@ git clone https://github.com/Supreme-Emperor-Wang/ImageAlchemy.git
 cd ~/ImageAlchemy
 pip install -r requirements.txt
 
-nohup python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name default --wallet.hotkey jackie_hotkey_24 --netuid 26 --subtensor.network local --axon.port 8102 --miner.device cuda:0 --blacklist.force_validator_permit true  >> hotkey24_output.log &
+nohup python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name default --wallet.hotkey jackie_hotkey_22 --netuid 26 --subtensor.network local --axon.port 8102 --miner.device cuda:0 --blacklist.force_validator_permit true  >> hotkey22_output.log &
+
+#in case mainnet failed
+#nohup python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name default --wallet.hotkey jackie_hotkey_22 --netuid 26 --subtensor.chain_endpoint ws://3.144.223.133:9944 --axon.port 8102 --miner.device cuda:0 --blacklist.force_validator_permit true  >> hotkey22_output.log &
+
 
 
