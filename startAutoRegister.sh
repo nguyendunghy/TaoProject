236b50ae756b9198b7da75aa9914935db7e6658a
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-18 07:51:52 +0700

    setup config register subnet 26

--- startAutoRegister.sh
+++ startAutoRegister.sh
@@ -1,2 +1,4 @@
-java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property >> logs/autoRegister.log &
+mvn clean
+mvn package
+java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property >> logs/hotkey26_autoRegister.log &
 #tail -100f logs/autoRegister.log
