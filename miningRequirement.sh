aa972003ea4a26b7843bf33605f8d41354b5ef47
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-19 00:05:43 +0700

    setup subnet 29

--- miningRequirement.sh
+++ miningRequirement.sh
@@ -6,24 +6,29 @@ pip3 install bittensor
 ./install-keys/install-coldkey.sh
 ./install-keys/install-hotkey-all.sh
 
-#install subtensor
-cd ~
-git clone https://github.com/opentensor/subtensor.git
-cd subtensor
-sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
-pwd
+  sudo apt-get update && sudo apt-get install redis-server
+sudo apt-get install libgl1-mesa-glx
+sudo apt-get install libglib2.0-0
 
-#install project
-cd ~
-git clone https://github.com/Supreme-Emperor-Wang/ImageAlchemy.git
-#git clone git@github.com:Supreme-Emperor-Wang/ImageAlchemy.git
-cd ~/ImageAlchemy
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+nvm install 18 && nvm use 18
+
+npm install pm2@latest -g
+
+git clone https://github.com/fractal-net/fractal.git
+cd fractal
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install 18 && nvm use 18
+npm install pm2@latest -g
+
+pip install -e .
 pip install -r requirements.txt
 
 #upgrade torch and torchvision
 pip install torch torchvision --upgrade
-cd ~/ImageAlchemy
-export PYTHONPATH=`pwd`
+
 
 #install new driver
 cd ..
