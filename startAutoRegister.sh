c2d3dd2a0ed0f2b67d54cf69bd3b9cac2376df66
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-19 17:58:54 +0700

    update register subnet 29

--- startAutoRegister.sh
+++ startAutoRegister.sh
@@ -1,4 +1,4 @@
 mvn clean
 mvn package
-java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property >> logs/autoRegister.log &
+java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property >> logs/subnet29_autoRegister.log &
 #tail -100f logs/autoRegister.log
