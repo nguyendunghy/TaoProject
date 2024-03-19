b2c203c43f17518e173e838a8ac635043d2e2157
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-19 10:06:59 +0700

    setup subnet 29

--- subnet-setup.sh
+++ subnet-setup.sh
@@ -1,14 +1,14 @@
-cd ~
-git clone https://github.com/Supreme-Emperor-Wang/ImageAlchemy.git
-#git clone git@github.com:Supreme-Emperor-Wang/ImageAlchemy.git
-cd ~/ImageAlchemy
-pip install -r requirements.txt
+"git clone https://github.com/fractal-net/fractal.git
+cd fractal"
+python3 -m venv venv
+source venv/bin/activate
+"pip install -e .
+pip install -r requirements.txt"
+pm2 start python3 -n server -- model/server.py
+pm2 start python3 -n dunghk2 -- neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:5005" --logging.debug --logging.trace --netuid 29 --axon.port 40353 --axon.external_port 40353 --wallet.name dungck1 --wallet.hotkey dunghk2
 
-#local
-nohup python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name default --wallet.hotkey jackie_hotkey_22 --netuid 26 --subtensor.network local --axon.port 8102 --miner.device cuda:0 --blacklist.force_validator_permit true  >> hotkey22_output.log &
+pm2 start python3 -n jackie -- neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:5005" --logging.debug --logging.trace --netuid 29 --axon.port 13609 --axon.external_port 13609 --wallet.name default --wallet.hotkey jackie_hotkey_25
 
-#main net
-#nohup python ~/ImageAlchemy/neurons/miners/StableMiner/main.py --wallet.name default --wallet.hotkey jackie_hotkey_23 --netuid 26 --subtensor.network finney --axon.port 2023 --miner.device cuda:0 --blacklist.force_validator_permit true  >> hotkey23_output.log &
 
 
 #in case mainnet failed
