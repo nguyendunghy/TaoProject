dd3b663e971bc62ca3886111786e48cf4afa0297
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-19 16:49:23 +0700

    setup subnet 26

--- miningRequirement.sh
+++ miningRequirement.sh
@@ -13,6 +13,7 @@ pip3 install bittensor
 #sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
 #pwd
 
+cd ~
 git clone https://github.com/Supreme-Emperor-Wang/ImageAlchemy.git
 sudo apt-get update
 sudo apt-get install python3.10-venv
