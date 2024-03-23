946e6e29ee680c5b6576d4c615d3eec06a66c224
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-23 07:49:06 +0700

    change script load all data

--- startLoadingC4.sh
+++ startLoadingC4.sh
@@ -1,3 +1,3 @@
 mvn clean
 mvn package
-java -jar target/TaoProject-1.0-SNAPSHOT.jar LOAD_C4 ./src/main/resources/application.property >> logs/loadc4.log &
+java -jar target/TaoProject-1.0-SNAPSHOT.jar LOAD_ALL_C4 ./src/main/resources/application.property >> logs/loadc4.log &
