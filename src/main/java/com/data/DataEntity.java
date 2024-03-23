f5d56c5d5ea4839ea6bcc58777dfa94234b2fff6
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-24 05:45:36 +0700

    update validator.json

--- src/main/java/com/data/DataEntity.java
+++ src/main/java/com/data/DataEntity.java
@@ -42,11 +42,24 @@ public class DataEntity {
     }
 
     public static void main(String[] args) {
-        String raw = "Beginners BBQ Class Taking Place in Missoula!\\nDo you want to get better at making delicious BBQ? You will have the opportunity, put this on your calendar now. Thursday, September 22nd join World Class BBQ Champion, Tony Balay from Lonestar Smoke Rangers. He will be teaching a beginner level class for everyone who wants to get better with their culinary skills.\\nHe will teach you everything you need to know to compete in a KCBS BBQ competition, including techniques, recipes, timelines, meat selection and trimming, plus smoker and fire information.\\nThe cost to be in the class is $35 per person, and for spectators it is free. Included in the cost will be either a t-shirt or apron and you will be tasting samples of each meat that is prepared.";
-        String sha256hex = DigestUtils.sha256Hex(raw);
-        System.out.println(sha256hex);
-        String txt = "df8fe77b749d3d27be290dc9a07ee522ab5d9fde1c523c0d3a18e1768efce3c3";
-        System.out.println(sha256hex.equals(txt));
-        System.out.println(Math.abs(txt.hashCode()) % 256);
+//        String raw = "Beginners BBQ Class Taking Place in Missoula!\\nDo you want to get better at making delicious BBQ? You will have the opportunity, put this on your calendar now. Thursday, September 22nd join World Class BBQ Champion, Tony Balay from Lonestar Smoke Rangers. He will be teaching a beginner level class for everyone who wants to get better with their culinary skills.\\nHe will teach you everything you need to know to compete in a KCBS BBQ competition, including techniques, recipes, timelines, meat selection and trimming, plus smoker and fire information.\\nThe cost to be in the class is $35 per person, and for spectators it is free. Included in the cost will be either a t-shirt or apron and you will be tasting samples of each meat that is prepared.";
+//        String sha256hex = DigestUtils.sha256Hex(raw);
+//        System.out.println(sha256hex);
+//        String txt = "df8fe77b749d3d27be290dc9a07ee522ab5d9fde1c523c0d3a18e1768efce3c3";
+//        System.out.println(sha256hex.equals(txt));
+//        System.out.println(Math.abs(txt.hashCode()) % 256);
+
+
+        char[] value = "df8fe77b749d3d27be290dc9a07ee522ab5d9fde1c523c0d3a18e1768efce3c3".toCharArray();
+        int h = 0;
+        if (h == 0 && value.length > 0) {
+            char val[] = value;
+
+            for (int i = 0; i < value.length; i++) {
+                h = 31 * h + val[i];
+            }
+        }
+        System.out.println(h);
+        System.out.println(h == "df8fe77b749d3d27be290dc9a07ee522ab5d9fde1c523c0d3a18e1768efce3c3".hashCode());
     }
 }
