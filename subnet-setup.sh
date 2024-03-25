9e11a8c56f4a8a827990420d5c0dd2697a5d54b9
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-25 22:38:44 +0700

    setup subnet 32

--- subnet-setup.sh
+++ subnet-setup.sh
@@ -8,6 +8,8 @@ nohup python3 neurons/miner.py --netuid 32 --wallet.name default --wallet.hotkey
 nohup python3 neurons/miner.py --netuid 32 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 1992 --subtensor.network local --blacklist.force_validator_permit true  >> hotkey23_output.log &
 
 
+nohup python3 neurons/miner.py --netuid 32 --wallet.name default --wallet.hotkey jackie_hotkey_24 --logging.debug --neuron.device cuda:0 --axon.port 6006  --axon.external_port 41014 --subtensor.network finney --blacklist.force_validator_permit true >> jackie_hotkey_24.log &
+
 #mainnet fail
 
 #nohup python3 neurons/miner.py --netuid 32 --wallet.name default --wallet.hotkey jackie_hotkey_22 --logging.debug --neuron.device cuda:0 --axon.port 2023 --subtensor.chain_endpoint ws://3.144.223.133:9944 --blacklist.force_validator_permit true >> hotkey22_output.log &
