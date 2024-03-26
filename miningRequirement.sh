fb0f48abfa4cc05e6aa5e1c1e54ab2bb394bcf6c
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-26 09:34:29 +0700

    setup subnet 32 on vast.io

--- miningRequirement.sh
+++ miningRequirement.sh
@@ -7,14 +7,14 @@ pip3 install bittensor
 ./install-keys/install-hotkey-all.sh
 
 #install subtensor
-cd ..
-git clone https://github.com/opentensor/subtensor.git
-cd subtensor
-sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
-pwd
+#cd ..
+#git clone https://github.com/opentensor/subtensor.git
+#cd subtensor
+#sudo ./scripts/run/subtensor.sh -e docker --network mainnet --node-type lite
+#pwd
 
 #install new driver
-cd ..
-sudo curl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/550.54.14/NVIDIA-Linux-x86_64-550.54.14.run
-sudo chmod 777 NVIDIA-Linux-x86_64-550.54.14.run
-sudo ./NVIDIA-Linux-x86_64-550.54.14.run
+#cd ..
+#sudo curl -O https://us.download.nvidia.com/XFree86/Linux-x86_64/550.54.14/NVIDIA-Linux-x86_64-550.54.14.run
+#sudo chmod 777 NVIDIA-Linux-x86_64-550.54.14.run
+#sudo ./NVIDIA-Linux-x86_64-550.54.14.run
