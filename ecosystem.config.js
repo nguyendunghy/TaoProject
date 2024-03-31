baab3096a49115a7857b495e10697b0605335e58
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-03-31 22:28:20 +0700

    upgrade setup subnet-29

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,8 @@ module.exports = {
             name: 'neurons-prover',
             script: 'python',
             interpreter: 'none',
-            args: 'neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:5005" --logging.debug --logging.trace --netuid 29 --axon.port 8888 --axon.external_port 6006 --wallet.name default --wallet.hotkey jackie_hotkey_25'
+            args: 'neurons/prover/app.py --subtensor.network finney --neuron.model_endpoint "http://127.0.0.1:5005" --logging.debug --logging.trace --netuid 29 --axon.port 7100 --axon.external_port 42895 --wallet.name default --wallet.hotkey jackie_hotkey_20'
         }
+
     ]
 };
