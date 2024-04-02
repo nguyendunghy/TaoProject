0a1624bb05071f550306db3eee977a65bb8f7025
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-02 15:00:06 +0700

    upgrade log check public ip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,13 @@ import org.example.utils.SystemUtils;
 public class Main {
     public static void main(String[] args) {
         try {
-            System.out.println("Public ip" + SystemUtils.getPublicIp());
+            System.out.println("Public IP address : " + SystemUtils.getPublicIp());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
         try {
-            System.out.println("IP socket" + SystemUtils.getPublicIpBySocket());
+            System.out.println("IP socket : " + SystemUtils.getPublicIpBySocket());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
