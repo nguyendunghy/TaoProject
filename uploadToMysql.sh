6b97e9ac138c5f810b069fe319a3572f785bf3af
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-09 13:25:31 +0700

    test upload datas

--- uploadToMysql.sh
+++ uploadToMysql.sh
@@ -23,7 +23,7 @@ for CSV_FILE in $CSV_DIR/*.csv; do
            IGNORE 0 ROWS;"
   
   # Execute the command
-  nohup time -f "%e seconds" mysql -u$USER -p$PASSWORD -h$HOST $DATABASE -e "$SQL_CMD" > "$CSV_FILE.log" &
+  nohup time -f "%e seconds" mysql -u$USER -p$PASSWORD -h$HOST $DATABASE -e "$SQL_CMD" > logs/"$CSV_FILE.log" &
   
   echo "Data from $CSV_FILE uploaded to $TABLE_NAME"
 done
