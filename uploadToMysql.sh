898bdf4a5d21b36de99ac6f9b1456eb0a5ceff8d
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-09 12:19:07 +0700

    uploadToMysql.sh added

---
+++ uploadToMysql.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# MySQL credentials
+USER="jackie"
+PASSWORD="jackie_password"
+DATABASE="ai_generated_text"
+HOST="localhost"
+PORT=3306
+TABLE_NAME="table_10000"
+# Directory containing your CSV files
+CSV_DIR="/home/ubuntu/mysql/processed_data"
+
+# Loop through each CSV file in the directory
+for CSV_FILE in $CSV_DIR/*.csv; do
+  # Extract the table name from the file name (removing path and .csv extension)
+#  TABLE_NAME=$(basename "$CSV_FILE" .csv)
+  
+  # Construct the MySQL LOAD DATA INFILE command
+  SQL_CMD="LOAD DATA INFILE '"$CSV_FILE"'
+           INTO TABLE $TABLE_NAME
+           FIELDS TERMINATED BY ','
+           ENCLOSED BY '\"'
+           LINES TERMINATED BY '\n'
+           IGNORE 0 ROWS;"
+  
+  # Execute the command
+  mysql -u$USER -p$PASSWORD -h$HOST $DATABASE -e "$SQL_CMD"
+  
+  echo "Data from $CSV_FILE uploaded to $TABLE_NAME"
+done
