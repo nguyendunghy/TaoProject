5520bae1069f14f1a1e33506b443e2ee60c8953e
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-11 04:56:20 +0700

    add param

--- merge.sh
+++ merge.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 
-GZ_DIR="/home/ubuntu/c4-dataset/indexed_data/c4-index-v1"
+C4_INDEX="/home/ubuntu/c4-dataset/c4-index-v1"
 
 # Loop through each CSV file in the directory
-for dir in $GZ_DIR/*; do
+for dir in C4_INDEX/*; do
   # Extract the table name from the file name (removing path and .csv extension)
   dir_name=$(basename "$dir")
   cd $dir
   paste -d',' *flush* > merged_$dir_name.txt
-
+  rm *flush*.txt
   echo "Data flush from $dir merged to merged_$dir_name.txt"
 done
