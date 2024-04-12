ae1d4b63059839ff5791e486f32e44ed7769945c
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-12 10:21:51 +0700

    unzip c4

--- unzip-c4.sh
+++ unzip-c4.sh
@@ -4,7 +4,8 @@
 C4_DATA_SET="/root/c4-dataset/c4/en"
 cd $C4_DATA_SET
 for dir in $C4_DATA_SET/*01024.json.gz; do
+  #unzip and remove file *.gz
+  #use gzip -d -k filename.gz to keep the *.gz files
   gzip -d $dir
-  rm $dir
   echo "unzip data from $dir success"
 done
