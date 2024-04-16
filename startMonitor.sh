accf19590f5eb7a2ffb6ab26b0966c78ea98d1d4
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-16 09:18:06 +0700

    monitor redis

--- startMonitor.sh
+++ startMonitor.sh
@@ -1,4 +1,4 @@
 mvn clean
 mvn package
 mkdir logs
-java -jar target/TaoProject-1.0-SNAPSHOT.jar MONITOR ./src/main/resources/application.property >> logs/monitor.log &
+java -jar target/TaoProject-1.0-SNAPSHOT.jar MONITOR ./src/main/resources/application.property >> logs/monitor-agent.log &
