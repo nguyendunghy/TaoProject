8586bd2176b600087607728c8a6e02c933b73258
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 23:12:36 +0700

    Test RunScript

--- startClientApp.sh
+++ startClientApp.sh
@@ -2,17 +2,11 @@
 
 # Directory containing the command files
 COMMAND_DIR="/home/ubuntu/TaoProject/script"
-
+TARGET_DIR="/home/ubuntu/TaoProject/processed"
+mkdir $COMMAND_DIR
+mkdir $TARGET_DIR
 # Continuously scan the directory
 while true; do
-    # Check if the directory exists
-    if [ ! -d "$COMMAND_DIR" ]; then
-        echo "Directory does not exist: $COMMAND_DIR"
-        # Optionally wait some time before retrying to avoid spamming the log/output
-        sleep 10
-        continue
-    fi
-
     # Loop through each file in the directory
     for command_file in "$COMMAND_DIR"/*; do
         # Check if the file is a regular file
@@ -23,6 +17,7 @@ while true; do
                 echo "Running: $cmd"
                 eval "$cmd"
             done < "$command_file"
+            mv "$command_file" "$TARGET_DIR"
         fi
     done
 
