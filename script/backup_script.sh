f7e5661838df6eca901d0f0f3cdfccc69dc5bf35
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 22:21:05 +0700

    Test RunScript

--- script/backup_script.sh
+++ script/backup_script.sh
@@ -2,6 +2,4 @@ mvn clean
 mvn package
 mkdir logs
 source /home/ubuntu/venv/head-tail/bin/activate
-cd /home/ubuntu/head-tail-llm-detection
-ls
-nohup python3 neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7100 --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &
+nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7100 --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &
