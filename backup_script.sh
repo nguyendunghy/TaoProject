887bdd261906c06dee7739da84fcbfd35e6c7004
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 23:34:31 +0700

    update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,6 +1,4 @@
-mvn clean
-mvn package
-mkdir logs
+echo "start running python app"
 source /home/ubuntu/venv/head-tail/bin/activate
 cd /home/ubuntu/head-tail-llm-detection
-java -jar /home/ubuntu/TaoProject/target/TaoProject-1.0-SNAPSHOT.jar BACKUP_SCRIPT /home/ubuntu/TaoProject/src/main/resources/application.property  /home/ubuntu/TaoProject/startClientApp.sh >> backup_scrip.log &
+nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_1 --logging.debug --neuron.device cuda:0 --axon.port 7101 --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_1.log &
