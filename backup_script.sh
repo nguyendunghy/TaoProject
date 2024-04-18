6f380bcbb3efa6473e0a4f9197a6297a1ccadcac
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 09:26:09 +0700

    test backup script

--- backup_script.sh
+++ backup_script.sh
@@ -1,4 +1,4 @@
 mvn clean
 mvn package
 mkdir logs
-java -jar target/TaoProject-1.0-SNAPSHOT.jar BACKUP_SCRIPT ./src/main/resources/application.property  "nohup python3 neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7101  --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &"
+java -jar target/TaoProject-1.0-SNAPSHOT.jar BACKUP_SCRIPT ./src/main/resources/application.property  "nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7101  --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &"
