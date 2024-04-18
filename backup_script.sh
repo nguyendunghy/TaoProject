1325b5958aeb7d16933d27bc4921b952559f8831
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 10:00:17 +0700

    test backup script

--- backup_script.sh
+++ backup_script.sh
@@ -3,4 +3,5 @@ mvn package
 mkdir logs
 source /home/ubuntu/venv/head-tail/bin/activate
 cd /home/ubuntu/head-tail-llm-detection
-java -jar /home/ubuntu/TaoProject/target/TaoProject-1.0-SNAPSHOT.jar BACKUP_SCRIPT /home/ubuntu/TaoProject/src/main/resources/application.property  "nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7101  --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &" >> backup_scrip.log &
+#java -jar /home/ubuntu/TaoProject/target/TaoProject-1.0-SNAPSHOT.jar BACKUP_SCRIPT /home/ubuntu/TaoProject/src/main/resources/application.property  "nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7101  --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &" >> backup_scrip.log &
+nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7101  --subtensor.network test --blacklist.minimum_stake_requirement 0 >> /home/ubuntu/head-tail-llm-detection/miner_hotkey_23.log &
