3f9a43f14351e572ed8d923efcd649d94e090b39
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-18 22:24:09 +0700

    Test RunScript

--- script/backup_script.sh
+++ script/backup_script.sh
@@ -1,5 +1,3 @@
-mvn clean
-mvn package
-mkdir logs
+echo "start running python app"
 source /home/ubuntu/venv/head-tail/bin/activate
 nohup python3 /home/ubuntu/head-tail-llm-detection/neurons/miner.py --netuid 87 --wallet.name default --wallet.hotkey jackie_hotkey_23 --logging.debug --neuron.device cuda:0 --axon.port 7100 --subtensor.network test --blacklist.minimum_stake_requirement 0 >> miner_hotkey_23.log &
