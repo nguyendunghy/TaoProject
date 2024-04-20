a8352772d85de8b4290a2b17b176ceddf5cae40f
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-04-20 10:15:06 +0700

    unzip c4-train.001*-of-01024.json.gz

--- unzip-c4.sh
+++ unzip-c4.sh
@@ -2,7 +2,7 @@
 
 
 C4_DATA_SET="/home/ubuntu/c4-dataset/c4/en"
-FILE_PATTERN="c4-train.01*-of-01024.json.gz"
+FILE_PATTERN="c4-train.001*-of-01024.json.gz"
 cd $C4_DATA_SET
 for dir in $C4_DATA_SET/$FILE_PATTERN; do
   #unzip and remove file *.gz
