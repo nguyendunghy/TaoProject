e18b79be6806f7480ad88ed533b73b4c0aa4d76a
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-26 09:34:35 +0700

    update main

--- src/main/java/org/example/utils/SystemUtils.java
+++ src/main/java/org/example/utils/SystemUtils.java
@@ -1,15 +1,36 @@
 package org.example.utils;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.URL;
 
 public class SystemUtils {
 
     public static String getIpAddress() throws Exception {
-        InetAddress inetAddress = InetAddress.getLocalHost();
+        return getPublicIp();
+    }
 
-        String ipAddress = inetAddress.getHostAddress();
+    public static String getPublicIp() throws Exception {
+        String urlString = "http://checkip.amazonaws.com/";
+        URL url = new URL(urlString);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            String ip = br.readLine();
+            System.out.println("getPublicIp: " + ip);
+            return ip;
+        }
+    }
 
-        System.out.println("IP Address of the running server: " + ipAddress);
-        return ipAddress;
+    public static String getPublicIpBySocket() throws Exception {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("google.com", 80));
+            return socket.getLocalAddress().getHostAddress();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(getPublicIp());
     }
 }
