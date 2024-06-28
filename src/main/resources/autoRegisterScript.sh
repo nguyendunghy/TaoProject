117130549502c78b1c3addf10dfdf416a2546be2
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-06-28 11:34:43 +0700

    add password

--- src/main/resources/autoRegisterScript.sh
+++ src/main/resources/autoRegisterScript.sh
@@ -28,6 +28,16 @@ expect {
 
 
 expect {
+    "Enter password*" {
+        send "$password\r"
+    }
+     timeout {
+        send_user "Expected password not found within timeout period.\n"
+        exit 1
+    }
+}
+
+expect {
     -re {Recycle τ([0-9.]+) to register on subnet:([0-9]+)\?} {
             set amount $expect_out(1,string)
             set amountFloat [expr {double($amount)}]
