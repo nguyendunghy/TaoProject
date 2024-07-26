f732899306407781187722e33c01e8d93ff39d70
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-07-26 17:53:33 +0700

    register subnet44

--- startAutoRegister.sh
+++ startAutoRegister.sh
@@ -1,5 +1,5 @@
 mvn clean
 mvn package
 mkdir logs
-java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property REG-SN35>> logs/autoRegister.log &
+java -jar target/TaoProject-1.0-SNAPSHOT.jar AUTO_REGISTER ./src/main/resources/application.property REG-SN44>> logs/autoRegister.log &
 #tail -100f logs/autoRegister.log
