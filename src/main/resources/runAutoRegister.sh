9e8a0007deb536ec3350e77a5951c734f17ef993
nan1009 <89458033+nan1009@users.noreply.github.com>
2024-07-26 17:38:12 +0700

    register subnet35

--- src/main/resources/runAutoRegister.sh
+++ src/main/resources/runAutoRegister.sh
@@ -4,5 +4,6 @@ subnetId=$1
 threshold=$2
 hotkey=$3
 coldkey=$4
+password=$5
 cd $5
-./autoRegisterScript.sh $subnetId $threshold $coldkey $hotkey
+./autoRegisterScript.sh $subnetId $threshold $coldkey $hotkey $password
